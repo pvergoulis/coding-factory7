bc9c8882b6e2aefc4f19d21db45c664c98b1ed02
Pavlos Vergoulis <vergoulispaulos@gmail.com>
2024-12-10 21:06:14 +0200

    Updated

--- src/gr/aueb/cf/ch2/BigIntApp.java
+++ src/gr/aueb/cf/ch2/BigIntApp.java
@@ -5,7 +5,7 @@ import java.math.BigInteger;
 public class BigIntApp {
 
     public static void main(String[] args) {
-        BigInteger bigNum1 = new BigInteger("1234567890987654321");
+        BigInteger bigNum1 = new BigInteger("1234567845690987654321");
         BigInteger bigNum2 = new BigInteger("1234356546532234");
         BigInteger result;
 
